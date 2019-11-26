ee60a7f76a8fbbfc570247775f3af36c42db9cae
Andrew Weihe <andrewy03@gmail.com>
2019-11-25 20:30:42 -0600

    generating search history

--- assets/script.js
+++ assets/script.js
@@ -9,3 +9,21 @@ $("#citySearchBtn").on("click", function(event) {
   getCurrentWeatherConditions(citySearched);
   getFiveDayForecast(citySearched);
 });
+
+// Showing previous searches in the sidebar.
+function populateSearchHistory() {
+    $("#searchHistoryList").empty();
+    let searchHistory = getStoredWeatherData().searchHistory;
+    if (searchHistory) {
+      for (let i = 0; i < searchHistory.length; i++) {
+        let item = $("<li class='list-group-item'></li>");
+        item.text(searchHistory[i].cityName);
+        $("#searchHistoryList").prepend(item);
+      }
+      $(".list-group-item").on("click", function() {
+        $("#searchResults").fadeIn("slow");
+        getCurrentWeatherConditions($(this).text());
+        getFiveDayForecast($(this).text());
+      });
+    }
+  }
